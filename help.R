071e77467658d95b680dae3fecf7d5210d79269e
hnosek <hunternosek@yahoo.com>
2021-06-10 16:51:06 -0500

    update

---
+++ help.R
@@ -0,0 +1,10 @@
+# Making connection
+# A connection is needed within RStudio and the airlines database for
+# the following problems. Here is how we would do it.
+
+
+# dbConnect(RMySQL::MySQL(),
+# dbname = "airlines",
+# host = "scidb.smith.edu",
+# user = "mth292",
+# password = "RememberPi")
